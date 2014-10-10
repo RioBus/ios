e0bad6f58a551a070e3c1a02482537b49eb2708a
Vitormm <vitor251093@gmail.com>
2014-10-09 23:47:49 -0300

    Link de Facebook finalizado
    Link de Facebook adicionado da mesma maneira que na versão para
    Android. Além disso, trechos de código não mais usados foram removidos.

--- riobus/OptionsViewController.m
+++ riobus/OptionsViewController.m
@@ -9,46 +9,26 @@
 #import "OptionsViewController.h"
 
 @interface OptionsViewController ()
-@property (strong, nonatomic) IBOutlet UIButton *buttonClose;
-@property (strong, nonatomic) IBOutlet UISegmentedControl *tupo;
-@property (strong, nonatomic) IBOutlet UISwitch *traf;
-@property (strong, nonatomic) IBOutlet UIButton *fbbutton;
-@property (weak,   nonatomic) IBOutlet UITextView *textView;
 @end
 
 @implementation OptionsViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    self.buttonClose.layer.cornerRadius = 10;
-    self.fbbutton.layer.cornerRadius = 10;
     // Do any additional setup after loading the view.
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    
-    [self.textView flashScrollIndicators];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)fb:(id)sender {
-    NSURL *url = [NSURL URLWithString:@"fb://profile/1408367169433222"];
-    
-    if ([[UIApplication sharedApplication] canOpenURL:url]) {
-        [[UIApplication sharedApplication] openURL:url];
-    }
-}
-
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
-    
     [self.delegate doneOptionsView];
 }
 
