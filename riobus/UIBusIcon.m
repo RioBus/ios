4c722f6441a2ddec8ca016053f835341d1453343
Mario Cecchi <macecchi@gmail.com>
2015-04-08 12:23:38 -0300

    Ícone do ônibus com suporte a retina

--- riobus/UIBusIcon.m
+++ riobus/UIBusIcon.m
@@ -14,14 +14,14 @@
 #define DEFAULT_FONT_SIZE   18
 
 @implementation UIImage (UIBusIconImage)
+
 -(UIImage*)imageByCombiningImage:(UIImage*)firstImage{
     UIImage *image = nil;
     
     CGSize newImageSize = CGSizeMake(self.size.width,self.size.height);
-    if (!UIGraphicsBeginImageContextWithOptions)
-         UIGraphicsBeginImageContextWithOptions(newImageSize, NO, [[UIScreen mainScreen] scale]);
-    else UIGraphicsBeginImageContext(newImageSize);
     
+    UIGraphicsBeginImageContextWithOptions(newImageSize, NO, 0.0);
+
     [self drawAtPoint:CGPointMake(roundf((newImageSize.width-self.size.width)/2),
                                         roundf((newImageSize.height-self.size.height)/2))];
     [firstImage drawAtPoint:CGPointMake(roundf((newImageSize.width-firstImage.size.width)/2),
@@ -31,6 +31,7 @@
     
     return image;
 }
+
 -(UIImage*)imageByMaskingImageWithColor:(UIColor*)color{
     CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
     
@@ -48,18 +49,15 @@
     
     return result;
 }
+
 -(UIImage*)imageByWritingText:(NSString*)text withColor:(UIColor*)textColor{
-    BOOL isRetina = ([[UIScreen mainScreen] respondsToSelector:@selector(displayLinkWithTarget:selector:)] &&
-                     ([UIScreen mainScreen].scale == 2.0));
-    NSInteger retinaScale = isRetina?1:2;
-    
-    UIFont *font = [UIFont boldSystemFontOfSize:DEFAULT_FONT_SIZE/retinaScale];
-    UIGraphicsBeginImageContext(self.size);
+    UIFont *font = [UIFont boldSystemFontOfSize:DEFAULT_FONT_SIZE];
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0);
     [self drawInRect:CGRectMake(0,0,self.size.width,self.size.height)];
     
     NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys:font, NSFontAttributeName, nil];
     CGFloat width = [[[NSAttributedString alloc] initWithString:text attributes:attributes] size].width;
-    CGRect rect = CGRectMake((self.size.width-width)/2, 4/retinaScale, width, self.size.height);
+    CGRect rect = CGRectMake((self.size.width-width)/2, 4, width, self.size.height);
     
     [text drawInRect:rect withAttributes:@{NSFontAttributeName:font, NSForegroundColorAttributeName:textColor}];
     UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
@@ -67,9 +65,11 @@
     
     return newImage;
 }
+
 @end
 
 @implementation UIColor (UIBusIconColor)
+
 -(UIColor*)foregroundColor{
     CGFloat grayScale;
     
@@ -88,9 +88,11 @@
     if (grayScale<0.5) return [UIColor whiteColor];
     else return [UIColor blackColor];
 }
+
 @end
 
 @implementation UIBusIcon
+
 +(UIImage*)iconForBusLine:(NSString*)busLine withDelay:(NSInteger)delayInformation andColor:(UIColor*)color{
     UIImage* imagem = [[UIImage imageNamed:@"bus-gray"] imageByMaskingImageWithColor:color];
     imagem = [imagem imageByWritingText:busLine withColor:[color foregroundColor]];
@@ -99,4 +101,5 @@
     else if (delayInformation > YELLOW_BORDER_LIMIT) return [imagem imageByCombiningImage:[UIImage imageNamed:@"bus-yellow"]];
     else                                             return [imagem imageByCombiningImage:[UIImage imageNamed:@"bus-green" ]];
 }
+
 @end
