f6687d7e2ae303c56812d59b6ec7a96283daac9d
Mario Cecchi <macecchi@gmail.com>
2015-04-14 19:54:06 -0300

    Refactor da tabela de onibus e opcao de limpar recentes

--- riobus/BusSuggestionsTable.h
+++ riobus/BusSuggestionsTable.h
@@ -10,8 +10,13 @@
 
 @interface BusSuggestionsTable : UITableView <UITableViewDelegate, UITableViewDataSource>
 
+-(void)updateUserRecentsList;
+-(void)updateUserFavoritesList;
+-(void)updateOptionsList;
+
 @property (strong, nonatomic) NSMutableArray* favorites;
 @property (strong, nonatomic) NSMutableArray* recents;
+@property (strong, nonatomic) NSMutableArray* options;
 
 -(void)addToRecentTable:(NSString*)newOne;
 
