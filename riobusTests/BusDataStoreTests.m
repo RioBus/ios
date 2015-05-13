255d94337029dec4976996039c20f0435325a5e8
Mario Cecchi <macecchi@gmail.com>
2015-05-13 20:27:41 -0300

    Adiciona timeout nos testes unitários dos serviços

--- riobusTests/BusDataStoreTests.m
+++ riobusTests/BusDataStoreTests.m
@@ -3,6 +3,8 @@
 
 #import "BusDataStore.h"
 
+#define TIMEOUT_SECONDS 10
+
 @interface BusDataStoreTests : XCTestCase
 
 @end
@@ -11,11 +13,9 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
@@ -30,69 +30,71 @@
  * Tests if the server is responding normally to a request with an empty line number
  */
 - (void)testLoadBusDataEmpty {
+    NSDate *timeout = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
     __block BOOL waitingForBlock = YES;
     
     [[BusDataStore sharedInstance] loadBusDataForLineNumber:@"" withCompletionHandler:^(NSArray *busesData, NSError *error) {
-        
         XCTAssertNil(busesData, @"busesData should've returned nil with empty line number");
         XCTAssertNotNil(error, @"Operation should have returned an error from server");
         
         waitingForBlock = NO;
-        
     }];
     
     // Run the loop
-    while (waitingForBlock) {
+    while (waitingForBlock && [timeout timeIntervalSinceNow] > 0) {
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
-                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
+                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:1]];
     }
+    
+    XCTAssertFalse(waitingForBlock, @"Test failed with timeout.");
 }
 
 /**
  * Tests if the server is responding normally to a request with an fake line number
  */
 - (void)testLoadBusDataFakeLine {
+    NSDate *timeout = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
     __block BOOL waitingForBlock = YES;
     
     [[BusDataStore sharedInstance] loadBusDataForLineNumber:@"ABCDEFGH" withCompletionHandler:^(NSArray *busesData, NSError *error) {
-        
         XCTAssertNotNil(busesData, @"busesData returned nil");
         XCTAssert(busesData.count == 0, @"busesData should've returned an empty array");
         XCTAssertNil(error, @"Operation returned an error");
         
         waitingForBlock = NO;
-        
     }];
     
     // Run the loop
-    while (waitingForBlock) {
+    while (waitingForBlock && [timeout timeIntervalSinceNow] > 0) {
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
-                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
+                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:1]];
     }
-}
+    
+    XCTAssertFalse(waitingForBlock, @"Test failed with timeout.");}
 
 /**
  * Tests if the server is responding normally to a request with an existing line number
  */
 - (void)testLoadBusDataRealLine {
+    NSDate *timeout = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
     __block BOOL waitingForBlock = YES;
     
     [[BusDataStore sharedInstance] loadBusDataForLineNumber:@"485" withCompletionHandler:^(NSArray *busesData, NSError *error) {
-        
         XCTAssertNotNil(busesData, @"busesData returned nil");
         XCTAssert(busesData.count > 0, @"busesData returned an empty array");
         XCTAssertEqualObjects([busesData[0] class], [BusData class], @"busesData does not contain a BusData object");
         XCTAssertNil(error, @"Operation returned an error");
         
         waitingForBlock = NO;
-        
     }];
     
     // Run the loop
-    while (waitingForBlock) {
+    while (waitingForBlock && [timeout timeIntervalSinceNow] > 0) {
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
-                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
+                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:1]];
     }
+    
+    XCTAssertFalse(waitingForBlock, @"Test failed with timeout.");
 }
 
 
@@ -101,22 +103,23 @@
  * Tests if the server is responding normally to a request with an empty line number
  */
 - (void)testLoadBusLineShapeEmpty {
+    NSDate *timeout = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
     __block BOOL waitingForBlock = YES;
     
     [[BusDataStore sharedInstance] loadBusLineShapeForLineNumber:@"" withCompletionHandler:^(NSArray *shapes, NSError *error) {
-        
-        XCTAssertNil(shapes, @"shapes should've returned nil with empty line number");
+        XCTAssertNil(shapes, @"Shapes should've returned nil with empty line number");
         XCTAssertNotNil(error, @"Operation should have returned an error from server");
         
         waitingForBlock = NO;
-        
     }];
     
     // Run the loop
-    while (waitingForBlock) {
+    while (waitingForBlock && [timeout timeIntervalSinceNow] > 0) {
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
-                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
+                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:1]];
     }
+    
+    XCTAssertFalse(waitingForBlock, @"Test failed with timeout.");
 }
 
 
@@ -124,51 +127,52 @@
  * Tests if the server is responding normally to a request with an empty line number
  */
 - (void)testLoadBusLineShapeFakeLine {
+    NSDate *timeout = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
     __block BOOL waitingForBlock = YES;
     
     [[BusDataStore sharedInstance] loadBusLineShapeForLineNumber:@"ABCDEFGH" withCompletionHandler:^(NSArray *shapes, NSError *error) {
-        
-        XCTAssertNotNil(shapes, @"shapes returned nil");
-        XCTAssert(shapes.count == 0, @"shapes should've returned an empty array");
+        XCTAssertNotNil(shapes, @"Shapes returned nil");
+        XCTAssert(shapes.count == 0, @"Shapes should've returned an empty array");
         XCTAssertNil(error, @"Operation returned an error");
         
-        
         waitingForBlock = NO;
-        
     }];
     
     // Run the loop
-    while (waitingForBlock) {
+    while (waitingForBlock && [timeout timeIntervalSinceNow] > 0) {
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
-                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
+                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:1]];
     }
+    
+    XCTAssertFalse(waitingForBlock, @"Test failed with timeout.");
 }
 
 /**
  * Tests if the server is responding normally to a request with an empty line number
  */
 - (void)testLoadBusLineShapeRealLine {
+    NSDate *timeout = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
     __block BOOL waitingForBlock = YES;
     
     [[BusDataStore sharedInstance] loadBusLineShapeForLineNumber:@"485" withCompletionHandler:^(NSArray *shapes, NSError *error) {
-        
-        XCTAssertNotNil(shapes, @"shapes returned nil");
-        XCTAssert(shapes.count > 0, @"shapes returned an empty array");
-        XCTAssert(((NSArray*)shapes[0]).count > 0, @"shapes does not contain a NSArray object");
+        XCTAssertNotNil(shapes, @"Shapes returned nil");
+        XCTAssert(shapes.count > 0, @"Shapes returned an empty array");
+        XCTAssert(((NSArray*)shapes[0]).count > 0, @"Shapes does not contain a NSArray object");
         NSArray* shape = shapes[0];
         CLLocation* location = shape[0];
-        XCTAssertNotNil(location, @"returned shape object is nil");
+        XCTAssertNotNil(location, @"Returned shape object is nil");
         XCTAssertNil(error, @"Operation returned an error");
         
         waitingForBlock = NO;
-        
     }];
     
     // Run the loop
-    while (waitingForBlock) {
+    while (waitingForBlock && [timeout timeIntervalSinceNow] > 0) {
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
-                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
+                                 beforeDate:[NSDate dateWithTimeIntervalSinceNow:1]];
     }
+    
+    XCTAssertFalse(waitingForBlock, @"Test failed with timeout.");
 }
 
 
--- riobusTests/BusDataTests.m
+++ riobusTests/BusDataTests.m
@@ -13,12 +13,10 @@
 
 - (void)setUp {
     [super setUp];
-    
     self.busData = [[BusData alloc] init];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
