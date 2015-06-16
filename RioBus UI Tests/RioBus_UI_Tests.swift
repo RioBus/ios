d749b105cf74631934a4d58a12a903f85a57a1f2
Mario Cecchi <macecchi@gmail.com>
2015-06-16 01:19:33 -0300

    Adiciona testes de UI pro iOS 9/Xcode 7

--- RioBus UI Tests/RioBus_UI_Tests.swift
+++ RioBus UI Tests/RioBus_UI_Tests.swift
@@ -5,34 +5,105 @@ class RioBus_UI_Tests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
+    func ignoreOkAlerts(app: XCUIApplication) {
+        if (app.alerts.count > 0) {
+            app.alerts.element.collectionViews.buttons["OK"].tap()
+        }
+    }
+    
+    func testAddSearchToTable() {
+        let app = XCUIApplication()
+        let searchField = app.searchFields["Pesquisar linha de ônibus"]
+
+        searchField.tap()
+        
+        searchField.typeText("324")
+        app.typeText("\r")
+        
+        // Ignorar possíveis erros da pesquisa que não são relevantes neste caso
+        ignoreOkAlerts(app);
+        
+        searchField.tap()
+        let newCell = app.tables.cells.containingType(.StaticText, identifier: "324").element
+        XCTAssert(newCell.exists, "Linha 324 não foi encontrada na tabela")
+    }
+    
+    func testMarkBusLineAsFavorite() {
+        let app = XCUIApplication()
+        let searchField = app.searchFields["Pesquisar linha de ônibus"]
+        searchField.tap()
+        
+        let favoriteCell = app.tables.cells.containingType(.Button, identifier: "StarFilled").element
+        let normalCells = app.tables.cells.containingType(.Button, identifier: "Star")
+        
+        // Se já existir uma linha favorita, removê-la
+        if (favoriteCell.exists) {
+            favoriteCell.buttons["StarFilled"].tap()
+            app.alerts.collectionViews.buttons["Excluir"].tap()
+            XCTAssert(!favoriteCell.exists, "Não removeu a linha favorita");
+        }
+        
+        // Garantir que exista pelo menos duas linhas normais na tabela
+        if (normalCells.count < 2) {
+            // Adicionar primeira
+            searchField.typeText("324")
+            app.typeText("\r")
+            // Ignorar possíveis erros da pesquisa que não são relevantes neste caso
+            ignoreOkAlerts(app);
+            searchField.tap()
+            
+            // Adicionar segunda
+            if (normalCells.count < 2) {
+                searchField.typeText("485")
+                app.typeText("\r")
+                // Ignorar possíveis erros da pesquisa que não são relevantes neste caso
+                ignoreOkAlerts(app);
+                searchField.tap()
+            }
+        }
+        
+        // Marcar uma linha como favorita
+        let normalCell = normalCells.elementAtIndex(0)
+        normalCell.buttons["Star"].tap()
+        XCTAssertEqual(favoriteCell.staticTexts.element.value as! String, normalCell.staticTexts.element.value as! String, "Não salvou a primeira linha como favorito")
+        
+        // Marcar outra linha como favorita
+        normalCell.buttons["Star"].tap()
+        app.alerts.collectionViews.buttons["Redefinir"].tap()
+        XCTAssertEqual(favoriteCell.staticTexts.element.value as! String, normalCell.staticTexts.element.value as! String, "Não salvou a segunda linha como favorito")
+        
+        // Remover dos favoritos
+        favoriteCell.buttons["StarFilled"].tap()
+        app.alerts.collectionViews.buttons["Excluir"].tap()
+        XCTAssert(!favoriteCell.exists, "Não removeu a linha favorita");
+    }
+    
+    func testBusLineSearchScreen() {
         let app = XCUIApplication()
         app.searchFields["Pesquisar linha de ônibus"].tap()
         
-        let pesquisarLinhaDeNibusSearchField = app.searchFields["Pesquisar linha de ônibus"]
-        pesquisarLinhaDeNibusSearchField.typeText("324")
+        let searchField = app.searchFields["Pesquisar linha de ônibus"]
+        searchField.typeText("324")
         app.typeText("\r")
-//        app.alerts["Erro"].collectionViews.buttons["OK"].tap() // FIXME: nao devia ter erro
-        pesquisarLinhaDeNibusSearchField.tap()
         
-        let suggestionTable = app.tables
-        let newCell = suggestionTable.cells["324"]
-        XCTAssertEqual(newCell.exists, true)
+        XCTAssert(app.staticTexts["324 - Ribeira X Castelo (Circular)"].exists, "Não exibiu a barra de informações")
+        XCTAssert(app.buttons["Ribeira"].exists, "Não exibiu o botão do sentido 1")
+        XCTAssert(app.buttons["Castelo"].exists, "Não exibiu o botão do sentido 2")
+        XCTAssertEqual(searchField.value as! String, "324", "Não atualizou a barra de pesquisa")
         
+    }
+    
+    func testClearFromTable() {
+        // TODO: adicionar elementos recentes na lista, limpar, verificar se limpou
+        XCTFail("Unimplemented")
     }
     
     func testOptionsScreen() {
