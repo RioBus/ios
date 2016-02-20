088c32bacd9632d25b6eace54ff7db1719cc15c8
Mario Cecchi <macecchi@gmail.com>
2016-02-19 23:12:33 -0200

    More refactors to BusLine and fixing test

--- riobus/BusLine.m
+++ riobus/BusLine.m
@@ -7,28 +7,31 @@
     if (self) {
         _name = name;
         
-        if (name && ![name isEqualToString:@""] && ![name isEqualToString:@"desconhecido"]) {
+        if (description.length != 0 && ![description isEqualToString:@"desconhecido"]) {
             _lineDescription = description.capitalizedString;
-            
-            // Enhance and parse line name removing parentheses and fetching destinations
-            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"s/\\[.*?\\]|\\(.*?\\)|\\{.*?\\}//g" options:NSRegularExpressionCaseInsensitive error:nil];
-            NSString *lineDescriptionWithoutParentheses = [regex stringByReplacingMatchesInString:_lineDescription options:0 range:NSMakeRange(0, _lineDescription.length) withTemplate:@""];
-            lineDescriptionWithoutParentheses = [lineDescriptionWithoutParentheses stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-            
-            // Parse the two locations from bus line description
-            NSMutableArray *places = [[lineDescriptionWithoutParentheses componentsSeparatedByString:@" X "] mutableCopy];
-            
-            // Only assign location property if we have parsed two names
-            if (places.count == 2) {
-                // Trim the name strings
-                for (int i=0; i<places.count; i++) {
-                    places[i] = [places[i] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-                }
-                _places = places;
-            }
+            [self parseOriginAndDestination];
         }
     }
     return self;
+}
+
+- (void)parseOriginAndDestination {
+    // Enhance and parse line name removing parentheses and fetching destinations
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"s/\\[.*?\\]|\\(.*?\\)|\\{.*?\\}//g" options:NSRegularExpressionCaseInsensitive error:nil];
+    NSString *lineDescriptionWithoutParentheses = [regex stringByReplacingMatchesInString:_lineDescription options:0 range:NSMakeRange(0, _lineDescription.length) withTemplate:@""];
+    lineDescriptionWithoutParentheses = [lineDescriptionWithoutParentheses stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    
+    // Parse the two locations from bus line description
+    NSMutableArray *places = [[lineDescriptionWithoutParentheses componentsSeparatedByString:@" X "] mutableCopy];
+    
+    // Only assign location property if we have parsed two names
+    if (places.count == 2) {
+        // Trim the name strings
+        for (int i=0; i<places.count; i++) {
+            places[i] = [places[i] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        }
+        _places = places;
+    }
 }
 
 @end
